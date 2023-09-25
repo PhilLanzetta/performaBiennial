cd7d66c171b7e48862f857f59f3048c3e21129bc
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-09-25 11:10:36 -0400

    Delete public directory

--- public/0b308cfb-3408c10e45b7c90a6025.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[827],{9274:function(t,e,a){a.d(e,{iq:function(){return u}});var r=a(4405);function u(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M8.25 3.75H19.5a.75.75 0 01.75.75v11.25a.75.75 0 01-1.5 0V6.31L5.03 20.03a.75.75 0 01-1.06-1.06L17.69 5.25H8.25a.75.75 0 010-1.5z",clipRule:"evenodd"}}]})(t)}}}]);
-//# sourceMappingURL=0b308cfb-3408c10e45b7c90a6025.js.map
