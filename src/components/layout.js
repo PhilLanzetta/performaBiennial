d2345c98b02e02e1274992fe4f9a6aefdcc7e209
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-09-27 16:01:55 -0400

    page transitions

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Layout = ({ children, location, handleCategoryFilter }) => {
   return (
     <>
       <Header location={location} handleCategoryFilter={handleCategoryFilter} />
-      <main className='main'>{children}</main>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <main className='main'>{children}</main>
+        </motion.div>
+      </AnimatePresence>
       <Footer location={location} handleCategoryFilter={handleCategoryFilter} />
     </>
   )
